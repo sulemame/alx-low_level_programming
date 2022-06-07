6e0cc84796f7264df85374fccf82a7ed9cd6eec4
sulemame <sulemame995@gmail.com>
2022-06-07 04:54:56 -0700

    0 put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,21 @@
 #include "holberton.h"
-
-
-
 /**
+ * main - Entry point
  *
- *   * main - Entry point
- *
- *     *
- *
- *       * Return: Always 0
- *
- *         */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	char h[] = "Holberton";
+	int m;
 
-		_putchar('H');
+	m = 0;
 
-			_putchar('o');
-
-				_putchar('l');
-
-					_putchar('b');
-
-						_putchar('e');
-
-							_putchar('r');
-
-								_putchar('t');
-
-									_putchar('o');
-
-										_putchar('n');
-
-											_putchar('\n');
-
-
-
-												return (0);
-
+	while (m <= 8)
+	{
+		_putchar(h[m]);
+		m++;
+	}
+	_putchar('\n');
+	return (0);
 }
