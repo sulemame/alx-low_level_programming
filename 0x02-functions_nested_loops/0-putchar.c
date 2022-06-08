8053ccbc345c4ce39f019ddb27c1297be4826d9a
sulemame <sulemame995@gmail.com>
2022-06-08 09:14:09 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
-#include "betty.h"
+#include "main.h"
 /**
- * main - Entry point
- *
+ * main - prints putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char h[] = "Holberton";
-	int m;
-
-	m = 0;
-
-	while (m <= 8)
-	{
-		_putchar(h[m]);
-		m++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
