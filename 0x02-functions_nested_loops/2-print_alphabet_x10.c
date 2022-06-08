05a5335946ab05fa309add1b31573ce7d5f49136
sulemame <sulemame995@gmail.com>
2022-06-08 12:07:30 -0700

    2 print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,130 +1,20 @@
-#include "stdio.h"
-
-
-int _putchar(char c);
-
-
+#include "main.h"
 
 /**
- *
- *  * print_times_table - print the times table until n
- *
- *   *
- *
- *    * @n: n times table
- *
- *     *
- *
- *      * Return: No return
- *
- *       */
+ *print alphabet_x10
+ */
 
-
-
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
+	int j;
+	int x;
 
-		int i;
-
-			int j;
-
-
-
-				if (n >= 0 && n <= 15)
-
-						{
-
-
-
-									for (i = 0; i <= n; i++)
-
-												{
-
-
-
-																for (j = 0; j <= n; j++)
-
-																				{
-
-
-
-																									if ((i * j) < 10)
-
-																														{
-
-																																				if (j != 0)
-
-																																										{
-
-																																																	_putchar(' ');
-
-																																																							_putchar(' ');
-
-																																																													_putchar(' ');
-
-																																																																		}
-
-																																									_putchar((j * i) + '0');
-
-																																													}
-
-
-
-																													else if ((i * j) < 100)
-
-																																		{
-
-																																								_putchar(' ');
-
-																																													_putchar(' ');
-
-																																																		_putchar((((j * i) / 10) % 10) + '0');
-
-																																																							_putchar(((j * i) % 10) + '0');
-
-																																																											}
-
-
-
-																																	else if ((i * j) < 1000)
-
-																																						{
-
-																																												_putchar(' ');
-
-																																																	_putchar((((j * i) / 100) % 10) + '0');
-
-																																																						_putchar((((j * i) / 10) % 10) + '0');
-
-																																																											_putchar(((j * i) % 10) + '0');
-
-																																																															}
-
-																																				if (j != (n))
-
-																																								{
-
-																																													_putchar(',');
-
-																																																}
-
-
-
-																																							}
-
-
-
-																		_putchar('\n');
-
-
-
-																				}
-
-
-
-										}
-
-
-
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
