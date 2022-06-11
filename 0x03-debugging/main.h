159436adfc61e6abdbfaabe266e1b2e8d4bfa286
sulemame <sulemame995@gmail.com>
2022-06-11 01:30:29 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,45 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
 
-#include <stdio>
+#include "main.h"
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-void print_times_day(int n);
 
-#endif
+
+/**
+ *
+ * * main - prints the largest of 3 integers
+ *
+ * * Return: 0
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	        int a, b, c;
+
+		        int largest;
+
+
+
+			        a = 972;
+
+				        b = -98;
+
+					        c = 0;
+
+
+
+						        largest = largest_number(a, b, c);
+
+
+
+							        printf("%d is the largest number\n", largest);
+
+
+
+								        return (0);
+
+}
